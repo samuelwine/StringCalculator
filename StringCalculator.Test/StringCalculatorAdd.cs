092d102c09013748b807cd6a2972468443a62750
Shmuel <samuelwine@gmail.com>
2019-12-18 12:00:17 +0000

    first commit

---
+++ StringCalculator.Test/StringCalculatorAdd.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+
+namespace StringCalculator.Test
+{
+    public class StringCalculatorAdd
+    {
+        public StringCalculator stringCalculator { get; set; } = new StringCalculator();
+
+        [Fact]
+        public void ReturnsZeroForEmptyString()
+        {
+
+        }
+    }
+}
