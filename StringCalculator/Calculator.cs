29eb198d98a9f1d0208b055d1de88d839f4b12fd
Shmuel <samuelwine@gmail.com>
2019-12-18 14:05:09 +0000

    second commit

---
+++ StringCalculator/Calculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringCalculator
+{
+    public class Calculator
+    {
+        public int Add(string numbers)
+        {
+            int result;
+            return result;
+        }
+    }
+}
